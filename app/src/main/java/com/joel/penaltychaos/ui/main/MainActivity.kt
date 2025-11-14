4f2ad4ce4f207662111639280d1f87e37964706e
Joel <joel.rodriguez@insbaixcamp.cat>
2025-11-14 17:28:35 +0100

    Commit Grid Incrementable, creació de Data class Item.

--- app/src/main/java/com/joel/penaltychaos/ui/main/MainActivity.kt
+++ app/src/main/java/com/joel/penaltychaos/ui/main/MainActivity.kt
@@ -1,17 +1,17 @@
-package com.joel.penaltychaos
+package com.joel.penaltychaos.ui.main
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.gridlayout.widget.GridLayout
-import android.view.View
 import com.google.android.material.button.MaterialButton
+import com.joel.penaltychaos.R
 import com.joel.penaltychaos.databinding.ActivityMainBinding
-import java.util.zip.Inflater
-
+import com.joel.penaltychaos.data.Item_cell
+import com.joel.penaltychaos.R.layout.item_cell
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,19 +33,27 @@ class MainActivity : AppCompatActivity() {
         val inflater = LayoutInflater.from(this)
         cells = mutableListOf()
 
+        val itemCellData = MutableList(9) { index -> Item_cell(index, false, "0") }
+
         // Generar 9 celdas
-        for (i in 0 until 9) {
+        itemCellData.forEach { item ->
             val cell = inflater.inflate(R.layout.item_cell, binding.boardGrid, false) as MaterialButton
             cell.id = View.generateViewId()
+
+            // Listener que actualiza modelo y vista
             cell.setOnClickListener {
-                if (cell.text.isEmpty()) {
-                    cell.text = "❌"
-                    cell.isEnabled = false
-                }
+                val numActual = item.value.toIntOrNull() ?: 0
+                item.value = (numActual + 1).toString()
+                cell.text = item.value
+                item.isUsed = true
             }
+
+            // Guardar referencia del botón en la data class
+            item.button = cell
+
+            // Añadir a la UI y lista de botones
             binding.boardGrid.addView(cell)
             cells.add(cell)
         }
     }
 }
-
