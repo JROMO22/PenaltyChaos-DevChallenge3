68162b9253a4df4b2cbc6a689610b98af57d86e6
Joel <joel.rodriguez@insbaixcamp.cat>
2025-11-14 17:28:49 +0100

    Commit Grid Incrementable, creació de Data class Item.

---
+++ app/src/main/java/com/joel/penaltychaos/data/item_cell.kt
@@ -0,0 +1,11 @@
+package com.joel.penaltychaos.data
+import com.google.android.material.button.MaterialButton
+
+data class Item_cell(
+    val index: Int,       // 0..8
+    var isUsed: Boolean,  // si ya se marcó
+    var value: String,    // "❌", "⭕", ""...
+    var button: MaterialButton? = null // referencia UI
+)
+
+
